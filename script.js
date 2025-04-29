d88f60c1e1aa6bbdea6b3f4a21c70a5714f2ba60
Eliassss-Sena <eliassena02@gmail.com>
2025-04-29 19:15:54 -0300

    mobile changes

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-let menu_icon = document.querySelector('.header-options-mobile')
-let menu_icon_item1 = document.querySelector('.header-options-mobile-item.a')
-let menu_icon_item2 = document.querySelector('.header-options-mobile-item.b')
-let menu_icon_item3 = document.querySelector('.header-options-mobile-item.c')
+let menu_icon = document.querySelector('.header-options-icon')
+let menu_icon_item1 = document.querySelector('.header-options-icon-item.a')
+let menu_icon_item2 = document.querySelector('.header-options-icon-item.b')
+let menu_icon_item3 = document.querySelector('.header-options-icon-item.c')
+let menu_mobile_options = document.querySelectorAll('.option-mobile')
+let menu_mobile_option1 = document.querySelector('.option-mobile.a')
+let menu_mobile_option2 = document.querySelector('.option-mobile.b')
+let menu_mobile_option3 = document.querySelector('.option-mobile.c')
+let menu_mobile_option4 = document.querySelector('.option-mobile.d')
+let menu_mobile_itens_text = document.querySelectorAll('.option-mobile a')
 
 menu_icon.addEventListener ('click', () => {
     const opacidade = window.getComputedStyle(menu_icon_item3).opacity;
@@ -9,10 +15,43 @@ menu_icon.addEventListener ('click', () => {
         menu_icon_item1.style.transform = 'rotate(40deg) translate(6px, 6px)'
         menu_icon_item2.style.transform = 'rotate(-40deg)'
         menu_icon_item3.style.opacity = 0
+        
+        menu_mobile_options.forEach(option => {
+            option.style.opacity = 1
+            option.style.border = '1px solid #9675d4'
+            option.style.borderRadius = '10px 0 0 10px'
+            option.style.backgroundColor = '#765ca565'
+        })
+        
+        menu_mobile_option1.style.width = '100px'
+        menu_mobile_option2.style.width = '75px'
+        menu_mobile_option3.style.width = '75px'
+        menu_mobile_option4.style.width = '60px'
+
+        menu_mobile_itens_text.forEach(option => {
+            option.style.fontSize = '13px'
+            option.style.transition = '0.5s ease 0.4s'
+        })
+        
     } else {
         menu_icon_item1.style.transform = 'none'
         menu_icon_item2.style.transform = 'none'
         menu_icon_item3.style.opacity = 1
+
+        menu_mobile_options.forEach(option => {
+            option.style.opacity = 0
+            option.style.border = '1px solid rgba(150, 117, 212, 0)'
+            option.style.borderRadius = '10px 0 0 10px'
+            option.style.backgroundColor = '#765ca565'
+        })
+
+        menu_mobile_option1.style.width = '0px'
+        menu_mobile_option2.style.width = '0px'
+        menu_mobile_option3.style.width = '0px'
+        menu_mobile_option4.style.width = '0px'
+
+        menu_mobile_itens_text.forEach(option => {
+            option.style.fontSize = '0px'
+        })
     }
-    
 })
